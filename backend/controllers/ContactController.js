1881a5fe003c1b0449b130c62b6307d11ffe5cd2
Yash Rajput <yashrajput9576@gmail.com>
2022-02-04 10:37:22 +0530

    Initial Commit

---
+++ backend/controllers/ContactController.js
@@ -0,0 +1,91 @@
+const Contact = require('../models/Contact')
+
+const listAll = (req, res) => {
+    Contact.find()
+    .then(response => {
+        res.json(response)
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: 'No Contacts found to Display!!'
+        })
+    })
+}
+
+const show = (req, res) => {
+    let contactId = req.body.contactId
+    Contact.findById(contactId)
+    .then(response => {
+        res.json(response)
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: 'Contact not Found!!'
+        })
+    })
+}
+
+const add = (req, res) => {
+    console.log(req.body);
+    let contact = new Contact(req.body)
+    contact.save()
+    .then(response => {
+        res.json({
+            message: 'Contact Added Successfully!!',
+            added_contact: response
+        })
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: 'Unable to add Contact some error occured!!'
+        })
+    })
+}
+
+const update = (req, res) => {
+    let contactId = req.body.contactId
+
+    let updatedContact = {
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email
+    }
+
+    Contact.findByIdAndUpdate(contactId, {$set: updatedContact})
+    .then(response => {
+        res.json({
+            message: 'Contact Updated Successfully!!',
+            updated_contact: updatedContact
+        })
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: 'Unable to update Contact some error occured!!'
+        })
+    })
+}
+
+const remove = (req, res) => {
+    let contactId = req.body.contactId
+
+    Contact.findByIdAndDelete(contactId)
+    .then(response => {
+        res.json({
+            message: 'Contact Deleted Successfully!!',
+            deleted_contact: response
+        })
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: 'Unable to delete Contact some error occured!!'
+        })
+    })
+}
+
+module.exports = {
+    listAll,
+    show,
+    add,
+    update,
+    remove
+}
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const ContactRouter = require('./routes/contact')
+
+mongoose.connect('mongodb://localhost:27017/contactdb')
+.then((result) => {
+    console.log('Connected to Database Successfully!!');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+app.use('/api/contact', ContactRouter)
