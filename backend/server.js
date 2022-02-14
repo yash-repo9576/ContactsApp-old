0f9a3d98596866f261f84c97ecc25b3566fc4915
Yash Rajput <yashrajput9576@gmail.com>
2022-02-14 07:52:02 +0530

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const ContactRouter = require('./routes/contact')
 
@@ -18,6 +19,8 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+app.use(cors())
 
 const PORT = 4000
 
